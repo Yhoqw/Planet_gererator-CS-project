a1546179a5926dbdb749a733a3cdf353d448bf8e
Yhoqw <yazdanalikhan005@gmail.com>
2024-12-07 19:34:15 +0500

    Add files via upload

--- PlanetGenerator_CS project.cpp
+++ PlanetGenerator_CS project.cpp
@@ -16,7 +16,6 @@ string name_parts[] = {"Zor", "Arg", "Tron", "Bel", "Nix", "Eld", "Omn", "Qua", 
 string atmospheres[] = {"Oxygen-rich", "Methane", "Carbon Dioxide", "Nitrogen", "Ammonia", "Helium"};
 string size_descriptions[] = {"Tiny", "Small", "Medium", "Large", "Massive"};
 
-string alien_
 
 //string for planet history prompts
 string histories[] = 
@@ -41,14 +40,19 @@ string races[] =
     "Mandalorians", "Shanghili", "Xenomorphs", "Vulcans", "Geth", "Jedi", "Turians" 
 };
 
+string appearances[] = {"Tall", "Short", "slender", "Reptilian", "Humanoid", "Insect-like"};
+string techLevels[] = {"Primitive", "Industrial", "Spacefaring", "Advanced AI", "Interdimensional"};
+string governments[] = {"Monarchy", "Republic", "Tribal council", "Technocracy", "Anarchy", "Theocracy"};
+
+
 // Function prototypes
 string generatePlanetName();
 string generateAtmosphere();
 string generateSize();
 void generateHistory(string history[], int &eventCount);
-void generateRaces(string raceList[], int &raceCount);
-void displayPlanet(const string &name, const string &atmosphere, const string &size, const string history[], int eventCount, const string raceList[], int raceCount);
-void save_planet_file(const string &name, const string &atmosphere, const string &size, const string history[], int eventCount, const string raceList[], int raceCount);
+void generateRaces(string raceList[], string raceDetails[], int &raceCount);
+void displayPlanet(const string &name, const string &atmosphere, const string &size, const string history[], int eventCount, const string raceList[], const string raceDetails[], int raceCount);
+void save_planet_file(const string &name, const string &atmosphere, const string &size, const string history[], int eventCount, const string raceList[], const string raceDetails[], int raceCount);
 void load_planet_file();
 
 int main() 
@@ -62,7 +66,7 @@ int main()
 	{
         cout << "Planet Generator" << endl;
         cout << "1. Explore a New Planet" << endl;
-        cout << "2. Explore an older Planet from File" << endl;
+        cout << "2. Explore previous Planet from File" << endl;
         cout << "3. Exit Program" << endl;
         cout << "Enter your choice: ";
         cin >> Input;
@@ -73,6 +77,7 @@ int main()
 			//case for planet Gereration
             case 1: 
 			{	
+                string planetName = generatePlanetName();			
                 string planetAtmosphere = generateAtmosphere();
                 string planetSize = generateSize();
 
@@ -83,12 +88,14 @@ int main()
 				
 				//Generate Races
                 string planetRaces[MAX_RACES];
+                string raceDetails[MAX_RACES];  
+				              
                 int raceCount = 0;
-                generateRaces(planetRaces, raceCount);
+                generateRaces(planetRaces, raceDetails, raceCount);
 
                 // Display and save the planet (using functions)
-                displayPlanet(planetName, planetAtmosphere, planetSize, planetHistory, historyEventCount, planetRaces, raceCount);
-                save_planet_file(planetName, planetAtmosphere, planetSize, planetHistory, historyEventCount, planetRaces, raceCount);
+                displayPlanet(planetName, planetAtmosphere, planetSize, planetHistory, historyEventCount, planetRaces, raceDetails, raceCount);
+                save_planet_file(planetName, planetAtmosphere, planetSize, planetHistory, historyEventCount, planetRaces, raceDetails, raceCount);
                 break;
             }
             
@@ -122,23 +129,31 @@ string generatePlanetName()
 {
     int parts = rand() % 3 + 2; // Between 2 and 4 parts
     string name = "";
+    
     for (int i = 0; i < parts; ++i) 
 	{
+		
         name += name_parts[rand() % MAX_NAME_PARTS];
+        
     }
+    
     return name;
 }
 
 // Function to generate a random atmosphere
 string generateAtmosphere() 
 {
+	
     return atmospheres[rand() % (sizeof(atmospheres) / sizeof(atmospheres[0]))];
+    
 }
 
 // Function to generate a random size
 string generateSize() 
 {
+	
     return size_descriptions[rand() % (sizeof(size_descriptions) / sizeof(size_descriptions[0]))];
+    
 }
 
 // Function to generate a random history
@@ -148,23 +163,31 @@ void generateHistory(string history[], int &eventCount)
     
     for (int i = 0; i < eventCount; ++i) 
 	{
-        history[i] = histories[rand() % (sizeof(histories) / sizeof(histories[0]))];
+		
+        history[i] = histories[rand() % (sizeof(histories) / sizeof(histories[0]))];       
     }
     
 }
 
 // Function to generate random races
-void generateRaces(string raceList[], int &raceCount) 
+void generateRaces(string raceList[], string raceDetails[], int &raceCount) 
 {
     raceCount = rand() % MAX_RACES + 1; // Between 1 and MAX_RACES
     for (int i = 0; i < raceCount; ++i) 
 	{
+		
         raceList[i] = races[rand() % (sizeof(races) / sizeof(races[0]))];
-    }
+        
+        //randmly generates (apearance, tech and government of alien races
+        raceDetails[i] = "Appearance { " + appearances[rand() % (sizeof(appearances) / sizeof(appearances[0]))] + " }, " +
+                         "Tech Level { " + techLevels[rand() % (sizeof(techLevels) / sizeof(techLevels[0]))] + " }, " +
+                         "Government { " + governments[rand() % (sizeof(governments) / sizeof(governments[0]))] + " }";		        
+    
+	}
 }
 
 // Function to display the planet details
-void displayPlanet(const string &name, const string &atmosphere, const string &size, const string history[], int eventCount, const string raceList[], int raceCount) 
+void displayPlanet(const string &name, const string &atmosphere, const string &size, const string history[], int eventCount, const string raceList[], const string raceDetails[], int raceCount) 
 {
     cout << "\n" "Planet Details" << endl;
     cout << "--------------" << endl;
@@ -175,18 +198,22 @@ void displayPlanet(const string &name, const string &atmosphere, const string &s
     cout << "\nHistory:" << endl;
     for (int i = 0; i < eventCount; ++i) 
 	{
+		
         cout << "- " << history[i] << endl;
+        
     }
 
     cout << "\nRaces:" << endl;
     for (int i = 0; i < raceCount; ++i) 
 	{
-        cout << "- " << raceList[i] << endl;
+		
+        cout << "- " << raceList[i] << ": " << raceDetails[i] << endl;
+	        
     }
 }
 
 // Function to save the planet details to a file
-void save_planet_file(const string &name, const string &atmosphere, const string &size, const string history[], int eventCount, const string raceList[], int raceCount) 
+void save_planet_file(const string &name, const string &atmosphere, const string &size, const string history[], int eventCount, const string raceList[], const string raceDetails[], int raceCount) 
 {
     ofstream outFile("planet.txt");
     if (outFile.is_open()) 
@@ -199,14 +226,19 @@ void save_planet_file(const string &name, const string &atmosphere, const string
         //for saving histories
         for (int i = 0; i < eventCount; ++i) 
 		{
+			
             outFile << history[i] << endl;
+            
         }
         
         //for saving races
         outFile << raceCount << endl;
         for (int i = 0; i < raceCount; ++i) 
 		{
+			
             outFile << raceList[i] << endl;
+           	outFile << raceDetails[i] << endl;           
+           	
         }
         
         outFile.close();
@@ -240,22 +272,29 @@ void load_planet_file()
         string history[MAX_HISTORY_EVENTS];
         for (int i = 0; i < eventCount; ++i) 
 		{
+			
             getline(inFile, history[i]);
+            
         }
 
         inFile >> raceCount;
         inFile.ignore(); // Ignore newline
 
         string raceList[MAX_RACES];
+        string raceDetails[MAX_RACES];       
+        
         for (int i = 0; i < raceCount; ++i) 
 		{
+			
             getline(inFile, raceList[i]);
+            getline(inFile, raceDetails[i]);
+            
         }
 
         inFile.close();
 
         // Display the loaded planet details
-        displayPlanet(name, atmosphere, size, history, eventCount, raceList, raceCount);
+        displayPlanet(name, atmosphere, size, history, eventCount, raceList, raceDetails, raceCount);
     }
 	 
 	else 
